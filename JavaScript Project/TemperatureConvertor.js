2cdb5def9bc59f28179843f28d02821fd5909f41
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-15 09:21:07 +0000

    Update TemperatureConvertor.js

--- JavaScript Project/TemperatureConvertor.js
+++ JavaScript Project/TemperatureConvertor.js
@@ -1,13 +1,17 @@
+//function for celsius to fahrenheit
 function CelsiusToFahrenheit(celsius){
     converted_fahrenheit = (celsius * 9/5) + 32
     console.log(`'${celsius}'°C is equal to '${converted_fahrenheit}'°F`)
 }
 
+// function for fahrenheit to celsius
 function FahrenheitToCelsius(fahrenheit){
     converted_celsius = (fahrenheit - 32) * 5/9
     console.log(`'${fahrenheit}'°C is equal to '${converted_celsius}'°F`)
 }
 
+
+//user input
 CelsiusTemperature = parseFloat(prompt("Temperature in Celsius: "))
 CelsiusToFahrenheit(CelsiusTemperature)
 
