f64cfdea5c5f52928ef7d0bd7ddbebe2197daef2
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-01-16 00:26:04 +0000

    Add files via upload

---
+++ PythonSortingAlgorithms/bubbleSort.py
@@ -0,0 +1,23 @@
+def bubble_sort(data, ascending=True):
+    #length of data
+    length = len(data)
+      #for lopp to iterate through the list (outer loop)
+    for i in range(length - 1):
+        #for loop iterate through the unsorted part of the list (inner loop)
+        for j in range(0, length - i - 1):
+            if ascending:
+                #if sorting is in ascending order
+                if data[j] > data[j + 1]:
+                     #swap the current element, if greater than the next element
+                    data[j], data[j + 1] = data[j + 1], data[j]
+            else:
+                #if sorting in descending order
+                if data[j] < data[j+1]:
+                    #swap the current elements is less than the next
+                    data[j], data[j + 1] = data[j+1], data[j]
+#list of numbers from 1 to 20 
+data = [x for x in range(1, 21)]
+
+bubble_sort(data, ascending=True)
+
+print("Sorted List (Ascending):", data)
